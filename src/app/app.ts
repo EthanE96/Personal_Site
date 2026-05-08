cbe403dd32a5c47bb55637a98fb2b6cd746812a5
Ethan <EthanAEdwards5@outlook.com>
2026-05-08 15:51:15 -0600

    Clarity

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './layout/sidebar/sidebar';
+import Clarity from '@microsoft/clarity';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.html',
 })
-export class App {}
+export class App {
+  constructor() {
+    const projectId = 'wo3b8v0vkh';
+    Clarity.init(projectId);
+  }
+}
